7bf04504f832eb3ecb0eedfe74ea203b072d2377
jindessi <96311944+Nomenandrianina@users.noreply.github.com>
2023-01-24 15:56:57 +0300

    User routes
    User routes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const userroutes = require("./routes/user_routes");
 
 app.get("/", (req,res) => {
     console.log('response from sir Antonio computer: ' + req.ip);
     res.send('response from sir Antonio computer: ' + req.ip);
 })
+
+app.use(userroutes);
 
 
 app.listen(port, () => console.log('Server app listening on port ' + port));
