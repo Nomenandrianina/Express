c68e4ff6c2ad7103aa0ecd16a447dc418f6d5b06
jindessi <96311944+Nomenandrianina@users.noreply.github.com>
2023-01-30 20:33:26 +0300

    Update server.js
    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,20 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const port = process.env.PORT || 8000;
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 app.use(cors(corsOptions)); 
 app.use(bodyParser.json({limit: '50mb'})); 
 app.use(bodyParser.urlencoded({
