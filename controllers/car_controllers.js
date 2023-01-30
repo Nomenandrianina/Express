ad99445b255de8def7629e76a7c23df12ff3d243
jindessi <96311944+Nomenandrianina@users.noreply.github.com>
2023-01-30 10:39:57 +0300

    Update all

--- controllers/car_controllers.js
+++ controllers/car_controllers.js
@@ -58,7 +58,7 @@ router.post("/create_car", async (req, res) => {
   router.post("/client/car/reparation", async (req, res) =>{
     try {
       const car = await Car.findOne({User: req.body.user});
-      const reparation = await Reparation.find({Car: car._id}).populate("Car");
+      const reparation = await Reparation.find({Car: car._id}).populate(["Car","Piece"]);
       res.status(200).json({ status:200,reparation,car });
     } catch (error) {
       res.status(400).json({ error });
